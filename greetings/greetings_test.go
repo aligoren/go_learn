b917767c014ae8eb7970053d802098f1332be0a5
Ali GOREN <goren.ali@yandex.com>
2021-02-24 20:33:28 +0300

    test added

---
+++ greetings/greetings_test.go
@@ -0,0 +1,28 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+
+	name := "Ali"
+
+	want := regexp.MustCompile(`\b` + name + `\b`)
+
+	message, err := Hello(name)
+
+	if !want.MatchString(message) || err != nil {
+		t.Fatalf(`Hello("Ali") = %q, %v, want match for %#q, nil`, message, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+
+	message, err := Hello("")
+
+	if message != "" || err == nil {
+		t.Fatalf(`Hello("") = %q, %v want "", error`, message, err)
+	}
+}
